71422065470e57a5b2fbec0793665ba2953e95a3
tobiastymkiw <tobiastymkiw@gmail.com>
2022-07-19 16:11:03 -0300

    perrito

--- PF-Rebirth/api/src/models/Owner.js
+++ PF-Rebirth/api/src/models/Owner.js
@@ -1,20 +1,21 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-    sequelize.define('owner', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        id: {
-            type: DataTypes.UUID,
-            allowNull: false,
-            primaryKey: true,
-            defaultValue: DataTypes.UUIDV4
-        },
-        image: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    })
-}
+  sequelize.define("owner", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    perrito: { type: DataTypes.STRING },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
